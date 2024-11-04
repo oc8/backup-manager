5b356d1fbe91ae69c3b1d9471efb0693be773582
X3ne <X3ne@users.noreply.github.com>
2024-11-04 16:15:53 +0100

    feat: update ssl certificate generation & add multi-databases support

--- generate_gobackup_config.sh
+++ generate_gobackup_config.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 
+# Start GoBackup configuration
 cat <<EOF > /etc/gobackup/gobackup.yml
 web:
   host: 0.0.0.0
@@ -25,12 +26,18 @@ models:
         path: ${MINIO_PATH}
         access_key_id: ${MINIO_ACCESS_KEY_ID}
         secret_access_key: ${MINIO_SECRET_ACCESS_KEY}
+EOF
+
+IFS=',' read -ra DBS <<< "${POSTGRES_DBS}"
+for DB_NAME in "${DBS[@]}"; do
+  cat <<EOF >> /etc/gobackup/gobackup.yml
     databases:
-      ${POSTGRES_DB}:
-        database: ${POSTGRES_DB}
+      ${DB_NAME}:
+        database: ${DB_NAME}
         type: postgresql
         host: localhost
         port: 5432
         username: ${POSTGRES_USER}
         password: ${POSTGRES_PASSWORD}
 EOF
+done
